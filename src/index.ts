306fb686fd1540c164c6900c69f862e6e3a10454
Sasha <sasha.hellstenius@gmail.com>
2019-01-31 12:56:18 +0100

    .#21 Fix testing on server Compile and run test files

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as morgan from 'morgan';
 import * as nodegit from 'nodegit';
 import * as path from 'path';
 import * as shell from 'shelljs';
+import * as fs from 'fs';
 
 const PORT = 3000;
 
@@ -42,20 +43,47 @@ app.post('/ci', async (req, res) => {
     });
 
   const srcPath = 'src';
-  const testPath = 'test';
-
   const lang = 'java';
-  const command = 'javac';
+  const compileCommand = 'javac';
+
+  const testPath = 'test';
+  const testCommand = 'java org.junit.runner.JUnitCore';
 
   // Compile
   await shell.exec(
-    `${command} ${directoryPath}/${srcPath}/*.${lang}`,
+    `${compileCommand} ${directoryPath}/${srcPath}/*.${lang}`,
     (code, stdout, stderr) => {
       console.log(`Code: ${code}`);
       console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr.length}`);
     },
   );
+
+  // Create dir for compiled test files
+  const testFolder = `${directoryPath}/testFolder`;
+
+  if (!fs.existsSync(testFolder)) {
+    fs.mkdirSync(testFolder);
+  }
+  // Compile test files and store in testFolder
+  await shell.exec(
+    `${compileCommand} ${directoryPath}/${testPath}/*.${lang} ${testFolder}`,
+    (code, stdout, stderr) => {
+      console.log(`Code: ${code}`);
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr.length}`);
+    },
+  );
+
+  // Run test files
+
+  await fs.readdirSync(testFolder).forEach((file) => {
+    shell.exec(`${testCommand} ${file}`, (code, stdout, stderr) => {
+      console.log(`Code: ${code}`);
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr.length}`);
+    });
+  });
 
   res.status(202);
 });
