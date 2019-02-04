5e6f69f1c234dd67a62e28c6a054c1ab6b1fbf9c
Sasha <sasha.hellstenius@gmail.com>
2019-02-04 14:38:36 +0100

    Fix #34 change from js test to ts testing

--- test/compile.test.js
+++ test/compile.test.js
@@ -1,22 +1,19 @@
-const compileCode = require('./../src/index.ts');
+const java = require('./../src/java');
 
 test('Test successfull compilation of source code', () => {
-  var result = compileCode("./dummy-code/src/MainSuccess.java");
+  var result = java("./dummy-code/src/MainSuccess.java");
     expect(result.success).toBe(true);
-    expect(result.type).toBe("compilation");
-    expect(result.message).toBe("");
+    expect(result.type).toBe("Compilation");
 });
 
 test('Test unsuccessfull compilationof source code', () => {
-  var result = compileCode("./dummy-code/src/MainFail.java");
+  var result = java("./dummy-code/src/MainFail.java");
     expect(result.success).toBe(false);
-    expect(result.type).toBe("compilation");
-    expect(result.message).toBe("class Main is public, should be declared in a file named Main.java public class Main {");
+    expect(result.type).toBe("Compilation");
 });
 
 test('Test successfull compilation of test code', () => {
-  var result = compileCode("./dummy-code/src/TestMainTrue.java");
+  var result = java("./dummy-code/src/TestMainTrue.java");
     expect(result.success).toBe(true);
-    expect(result.type).toBe("compilation");
-    expect(result.message).toBe("");
+    expect(result.type).toBe("Compilation");
 });
--- test/test.test.js
+++ test/test.test.js
@@ -1,20 +1,19 @@
-const testCode = require('./../src/index.ts');
-const compileCode = require('./../src/index.ts');
+const java = require('./../src/java');
 
 // Compile tests before running them
-compileCode("./dummy-code/src/TestMainTrue.java");
-compileCode("./dummy-code/src/TestMainFalse.java");
+java.compileCode("./dummy-code/src/TestMainTrue.java");
+java.compileCode("./dummy-code/src/TestMainFalse.java");
 
 
 test('Test successfull test', () => {
-  var result = testCode("./dummy-code/src/TestMainTrue");
+  var result = java.testCode("./dummy-code/src/TestMainTrue");
     expect(result.success).toBe(true);
     expect(result.type).toBe("test");
     // expect(result.message).toBe("");
 });
 
 test('Test unsuccessfull compilation', () => {
-  var result = testCode("./dummy-code/src/TestMainFalse");
+  var result = java.testCode("./dummy-code/src/TestMainFalse");
     expect(result.success).toBe(false);
     expect(result.type).toBe("test");
     // expect(result.message).toBe("");
