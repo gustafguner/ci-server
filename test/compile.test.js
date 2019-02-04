66e8342bd29b772235b0e0226c2e6a3aa2a62360
Sasha <sasha.hellstenius@gmail.com>
2019-02-04 11:22:37 +0100

    Fix #34 Add test for compiling test code Ensure that compilation works for test code and not only source code

--- test/compile.test.js
+++ test/compile.test.js
@@ -1,15 +1,22 @@
 const compileCode = require('./../src/index.ts');
 
-test('Test successfull compilation', () => {
+test('Test successfull compilation of source code', () => {
   var result = compileCode("./dummy-code/src/MainSuccess.java");
     expect(result.success).toBe(true);
     expect(result.type).toBe("compilation");
     expect(result.message).toBe("");
 });
 
-test('Test unsuccessfull compilation', () => {
+test('Test unsuccessfull compilationof source code', () => {
   var result = compileCode("./dummy-code/src/MainFail.java");
     expect(result.success).toBe(false);
     expect(result.type).toBe("compilation");
     expect(result.message).toBe("class Main is public, should be declared in a file named Main.java public class Main {");
+});
+
+test('Test successfull compilation of test code', () => {
+  var result = compileCode("./dummy-code/src/TestMainTrue.java");
+    expect(result.success).toBe(true);
+    expect(result.type).toBe("compilation");
+    expect(result.message).toBe("");
 });
