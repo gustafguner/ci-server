f99c25d5265cb24e27aec5558a96c94719091fd7
Gustaf Gunér <gustaf@guner.se>
2019-02-07 17:07:49 +0100

    Test commit

--- dummy-code/test/TestMainFalse.java
+++ dummy-code/test/TestMainFalse.java
@@ -1,13 +1,10 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TestMainFalse {
-
 	@Test
-	public void testThatIsFalse () {
+	public void testThatIsFalse() {
 		Main main = new Main();
 		assertEquals(main.max(1, 0), 0);
 	}
-
 }
--- dummy-code/test/TestMainTrue.java
+++ dummy-code/test/TestMainTrue.java
@@ -1,11 +1,9 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TestMainTrue {
-
 	@Test
-	public void testThatIsTrue(){
+	public void testThatIsTrue() {
 		Main main = new Main();
 		assertEquals(main.max(1, 0), 1);
 	}
